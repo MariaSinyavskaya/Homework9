915dd4f1231d6f02a5a78194c23d124572514c64
Мария <maria.sinyavskaya8@gmail.com>
2023-08-02 15:09:18 +0300

    Сделано ДЗ

--- src/Main.java
+++ src/Main.java
@@ -62,7 +62,6 @@ public class Main {
         for (int i = reverseFullName.length - 1; i >= 0; i--) {
             System.out.print(reverseFullName[i]);
         }
-        System.out.println("\n==================================================================================");
     }
 }
 
